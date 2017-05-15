715bcdc1833e5fb5e0e18029f7c19dd999a75127
David Vargas <dvargas92495@github.com>
2017-05-14 22:50:28 -0400

    returning objects in getFileNames()

--- src/detect.js
+++ src/detect.js
@@ -1,3 +1,29 @@
+var storj = require('storj-lib');
+var fs = require('fs');
+
+var api = 'https://api.storj.io';
+var keypair = storj.KeyPair(fs.readFileSync('./private.key').toString());//'./keys/' + cli.getKey()).toString());
+var client = storj.BridgeClient(api, {keyPair: keypair});
+
 module.exports.getFileNames = function(){
-	return ['data/hello.txt']
+	client.getBuckets(function(err, buckets) {
+	  if (err) {
+	    // Handle error on failure.
+	    return console.log('error', err.message);
+	  }
+
+	  if (!buckets.length) {
+	    return console.log('warn', 'You have not created any buckets.');
+	  }
+
+	  // Log out info for each bucket
+	  buckets.forEach(function(bucket) {
+	    console.log(
+	      'info',
+	      'ID: %s, Name: %s, Storage: %s, Transfer: %s',
+	      bucket.id, bucket.name, bucket.storage, bucket.transfer
+	    );
+	  });
+	});
+	return [{path:'data/hello.txt',bucket:0}]
 }
